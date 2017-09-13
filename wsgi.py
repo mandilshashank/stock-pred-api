3f45150c09ee1cceabf8c2770256f61b22235178
Ubuntu <ubuntu@ip-172-31-33-64.us-west-2.compute.internal>
2017-09-13 08:06:47 +0000

    Adding the wsgi files for stock-pred-api

---
+++ wsgi.py
@@ -0,0 +1,9 @@
+import sys
+import os
+sys.path.append('/home/ubuntu/stock-pred/src')
+
+from StockApi.Api import app as application
+
+#def application(environ, start_response):
+#    start_response('200 OK', [('Content-Type', 'text/html')])
+#    return ["<h1 style='color:blue'>Hello There!</h1>"]
